83f27e4b5bfbf27c1b675575f4d0d295c1010898
Mark Oliver <mark.c.oliver@gmail.com>
2019-08-22 20:15:43 -0400

    employee

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,75 @@
 require 'rails_helper'
 
+Tfname = "tfname"
+Tlname = "tlname"
+
+def make_employee(first_name = Tfname, last_name = Tlname, rewards_balance = nil)
+  Employee.new({first_name: first_name, last_name: last_name, rewards_balance: rewards_balance})
+end
+
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do 
+    it "is valid" do
+      employee = make_employee
+
+      result = employee.valid?
+      errors = employee.errors.full_messages
+
+      expect(result).to be(true)
+      expect(errors).to be_empty
+    end
+    it "is invalid without a first name" do 
+      employee = make_employee nil
+
+      result = employee.valid?
+      errors = employee.errors.full_messages
+
+      expect(result).to be(false)
+      expect(errors).to include("First name can't be blank")
+    end
+    it "is invalid without a last name" do
+      employee = make_employee Tfname, nil
+
+      result = employee.valid?
+      errors = employee.errors.full_messages
+
+      expect(result).to be(false)
+      expect(errors).to include("Last name can't be blank")      
+    end
+  end
+  context "attributes" do 
+    it "has expected attributes" do
+      employee = make_employee
+
+      result = employee.attribute_names.map(&:to_sym)
+
+      expect(result).to contain_exactly(
+        :id,
+        :first_name,
+        :last_name,
+        :rewards_balance,
+        :created_at,
+        :updated_at
+      )
+    end  
+  end
+  context "scopes" do 
+    describe ".zero_balance" do
+      before do
+        Employee.create!([
+          {first_name: :neg1, last_name: Tlname, rewards_balance: -1 },
+          {first_name: :zero, last_name: Tlname, rewards_balance: 0 },
+          {first_name: :one, last_name: Tlname, rewards_balance: 1 }          
+        ]).sort_by(&:rewards_balance)
+      end
+      it "returns a list of zero or less balance" do
+        result = Employee.zero_balance
+
+        expect(result.count).to eq 2
+        expect(result.first.first_name).to eq("neg1")
+        expect(result.last.first_name).to eq("zero")
+        expect(result.any? {|employee| employee.first_name == "one" }).to be(false)
+      end
+    end
+  end   
 end
