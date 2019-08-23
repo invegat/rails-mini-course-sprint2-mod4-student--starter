0bcc5c3ed2bcd7308862f33ae98fce8578cb792a
Mark Oliver <mark.c.oliver@gmail.com>
2019-08-22 23:21:10 -0400

    rewards_report

--- spec/models/rewards_report_spec.rb
+++ spec/models/rewards_report_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe RewardsReport do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#notify" do
+    let(:rewards) { [
+      double("reward", purchase_count: 30, id: 1),
+      double("reward", purchase_count: 70, id: 2),
+      double("reward", purchase_count: 100, id: 3)              
+    ]}
+    it "should total the purchase_count values" do
+      allow(NotificationService).to receive(:send_purchase_report)
+
+      rr = RewardsReport.new(rewards)
+      rr.notify()
+
+      expect(NotificationService).to have_received(:send_purchase_report).with(200) 
+    end
+  end
 end
